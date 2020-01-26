4ad7b76f1ce9a9fb6db7035d7767b1e7578f6d56
Jb Piacentino <jb@edtechone.com>
2020-01-26 12:31:06 +0100

    link to tb-webfront.test + README.md

---
+++ README.md
@@ -0,0 +1,29 @@
+# Extension Territoire Plus
+
+
+
+### `npm run build`
+
+Build the extension into `dist` folder for **production**.
+
+### `npm run build:dev`
+
+Build the extension into `dist` folder for **development**.
+
+### `npm run watch`
+
+Watch for modifications then run `npm run build`.
+
+### `npm run watch:dev`
+
+Watch for modifications then run `npm run build:dev`.
+
+It also enable [Hot Module Reloading](https://webpack.js.org/concepts/hot-module-replacement), thanks to [webpack-extension-reloader](https://github.com/rubenspgcavalcante/webpack-extension-reloader) plugin.
+
+:warning: Keep in mind that HMR only works for your **background** entry.
+
+### `npm run build-zip`
+
+Build a zip file following this format `<name>-v<version>.zip`, by reading `name` and `version` from `manifest.json` file.
+Zip file is located in `dist-zip` folder.
+
--- src/inject.js
+++ src/inject.js
@@ -1,33 +1,41 @@
 logoUrl = browser.runtime.getURL("assets/icons/tp-icon-24.png");
 console.log('logoUrl=', logoUrl);
-searchUrl="http://tp-webfront.test/search?query=macron";
 
-document.getElementById("hdtb-msb").innerHTML+= `
-	<div id="tp_item" class="hdtb-mitem hdtb-imb" aria-hidden="true">
- 		<a class="q qs" href="`+ searchUrl + `">
- 		<span style="height:16px;width:16px;display:inline-block;fill:currentColor;margin-right:5px;vertical-align:text-bottom;">
- 			<img src="`+ logoUrl + `" alt="" data-atf="1">
- 		</span>
-	</div> `;
+searchUrl="http://tp-webfront.test";
+//searchUrl=" ";
+
+document.getElementById("hdtb-msb").innerHTML+= 
+	`<div id="tp_item" class="hdtb-mitem hdtb-imb" aria-hidden="true">
+ 		<a id="tp_anchor" class="q qs" href="`+ searchUrl + `">
+ 			<span style="height:16px;width:16px;display:inline-block;fill:currentColor;margin-right:5px;vertical-align:text-bottom;">
+ 				<img src="`+ logoUrl + `" alt="" data-atf="1">
+ 			</span>
+ 		</a>
+	</div>`;
 
 if (document.getElementById("tp_item")) {
 	document.getElementById("tp_item").addEventListener("click", showTp); 
 }
 else console.log('not adding click');
 
-function notifyExtension(e) 
-{
+// function notifyExtension(e) 
+// {
 
-	var target = e.target;
+// 	var target = e.target;
 
-	console.log("content script sending message");
-  	browser.runtime.sendMessage(e);
-}
-
+// 	console.log("content script sending message");
+//   	browser.runtime.sendMessage(e);
+// }
 
 function showTp(e)
 {
-	
+	queryString=document.getElementsByName("q")[0].value;
+	document.getElementById("tp_anchor").href="http://tp-webfront.test/search?query=" + queryString;
+
+	// var query = document.getElementsByName("q");
+
+	// console.log('query =', query);
+
 	//notifyExtension({'type':'territoire-plus', 'message': 'click'});
 
 	// console.log('showTp');
@@ -43,11 +51,20 @@ function showTp(e)
 	//httpGetAsync("http://example.com", showContent);
 }
 
-function showContent(content)
-{
-	console.log('showContent',content);
-	document.body.textContent = "";
-	// var header = document.createElement('h1');
-	// header.textContent = "This page has been eaten";
-	document.body.appendChild(content);
-}
+// function showContent(content)
+// {
+// 	console.log('showContent',content);
+// 	document.body.textContent = "";
+// 	// var header = document.createElement('h1');
+// 	// header.textContent = "This page has been eaten";
+// 	document.body.appendChild(content);
+// }
+
+// function getUrlParams(search) {
+//   let hashes = search.slice(search.indexOf('?') + 1).split('&')
+//   return hashes.reduce((params, hash) => {
+//       let [key, val] = hash.split('=')
+//       return Object.assign(params, {[key]: decodeURIComponent(val)})
+//   }, {})
+//}
+
