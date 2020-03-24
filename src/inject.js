f840ceb53f15bb1c3cf3582fc0101e40785c7f9f
Jb Piacentino <jb@edtechone.com>
2020-03-24 23:31:12 +0100

    --allow-empty

--- src/inject.js
+++ src/inject.js
@@ -1,18 +1,17 @@
-logoUrl = browser.runtime.getURL('assets/icons/tp-icon-24.png');
-
-searchUrl = 'http://tp-webfront.test';
-
+// On insere un texte Confiance dans la toolbar
 document.getElementById('hdtb-msb').innerHTML += `<div id="tp_item" class="hdtb-mitem hdtb-imb" aria-hidden="true">
   		<a id="tp_anchor" class="q qs" href="#">
 			Confiance
 		</a>
 	</div>`;
 
+// On met un listener sur ce texte
 if (document.getElementById('tp_item')) {
   document.getElementById('tp_item').addEventListener('click', showTp);
 } else console.log('not adding click');
 
 function showTp(e) {
+  // Le texte a été cliqué
   queryString = document.getElementsByName('q')[0].value;
 
   console.log('query =', queryString);
